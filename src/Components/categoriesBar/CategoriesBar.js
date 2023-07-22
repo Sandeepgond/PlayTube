0abaef3c688958c9fc5bfc8536426c0cb6b7602a
Sandeepgond <sandeepgond13sandy@gmail.com>
2023-07-22 15:15:53 +0530

    categories bar layout done

--- src/Components/categoriesBar/CategoriesBar.js
+++ src/Components/categoriesBar/CategoriesBar.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./_categoriesBar.scss"
 
+const keywords=[
+  "All",
+  "React js",
+  "Angular js",
+  "Use of Api",
+  "React Native",
+  "Redux",
+  "Music",
+  "Guitar",
+  "Bollywood songs",
+  "Coding",
+  "Cricket",
+  "FootBall",
+  "Real Madrid",
+  "MS Dhoni"
+]
+
 const CategoriesBar = () => {
+  const [activeElement,setActiveElement]=useState("All")
+
+  const handleClick=(value)=>{
+    setActiveElement(value)
+  }
   return (
-    <div>CategoriesBar</div>
+    <div className='CategoriesBar'>
+      {
+        keywords.map((value,ind)=>(
+          <span onClick={()=>handleClick(value)} key={ind} 
+            className={activeElement===value?"active":""}
+          >
+            {value}
+          </span>
+        ))
+      }
+    </div>
   )
 }
 
