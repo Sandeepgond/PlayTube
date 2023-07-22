44b306a25cb3e8fa991ca205502774e459f5bd0c
Sandeepgond <sandeepgond13sandy@gmail.com>
2023-07-22 17:25:22 +0530

    Routes created successfully

--- src/App.js
+++ src/App.js
@@ -6,24 +6,40 @@ import {Container} from "react-bootstrap"
 import HomeScreen from "./screens/homescreen/HomeScreen"
 import LoginScreen from "./screens/loginscreen/LoginScreen"
 
-function App() {
-  
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+
+
+const Layout=({children})=>{
   const [toggleSideBar,setToggleSideBar]=useState(false)
-
   const handleToggleSideBar=()=>setToggleSideBar(value=>!value)
-
+  
   return (
     <>
-      {/* <Headers handleToggleSideBar={handleToggleSideBar} />
-      <div className="app_container border border-info" >
+      <Headers handleToggleSideBar={handleToggleSideBar} />
+      <div className="app_container" >
         <Sidebar toggleSideBar={toggleSideBar} handleToggleSideBar={handleToggleSideBar} />
-        <Container fluid className="app_main border border-warning">
-          <HomeScreen/>
+        <Container fluid className="app_main">
+          {children} 
         </Container>
-      </div> */}
-      <LoginScreen/>
+      </div>
     </>
-  );
+  )
+  
+}
+
+function App() {
+  // let navigate = useNavigate(); 
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><HomeScreen/></Layout>}/>
+        <Route path="/login" element={<LoginScreen/>}/>
+        <Route path="/search" element={<Layout><h1>Search Results</h1></Layout>}/>
+        <Route path="*" element={<h1>Path Not Found</h1>}/>
+      </Routes>
+    </Router>
+  )
+  
 }
 
 export default App;
