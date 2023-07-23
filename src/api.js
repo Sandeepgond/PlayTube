fa57a081e29a97f23ec38074e33057f7514e3ab8
Sandeepgond <sandeepgond13sandy@gmail.com>
2023-07-23 21:31:22 +0530

    Home Screen Video Fetched Successfully

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ import axios from "axios"
 const request= axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params:{
-        // key:process.env.REACT_APP_YouTube_Api_Key
         key:'AIzaSyBD3XPRMzTGcV6LOYriAsgnALbc0SZ43RM'
     }
 })
