f0c375ab17727cb229b2228d0bd46b41b7a0dcbb
Sandeepgond <sandeepgond13sandy@gmail.com>
2023-08-08 16:20:33 +0530

    key not diplayed

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios"
+const key=process.env.YouTube_Api_Key
 
 const request= axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params:{
-        key:'AIzaSyBD3XPRMzTGcV6LOYriAsgnALbc0SZ43RM'
+        key:key
     }
 })
 
