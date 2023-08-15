d3461c0487abdb21e773321d838c5a86709ccdc0
Sandeepgond <sandeepgond13sandy@gmail.com>
2023-08-15 18:28:22 +0530

    deploy

--- src/Components/header/Headers.js
+++ src/Components/header/Headers.js
@@ -16,10 +16,9 @@ const Headers = ({ handleToggleSideBar }) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     navigate(`/search/${input}`)
-    // setInput("")
   }
 
-  const {photoURL}=useSelector(state=>state.auth?.user)
+  // const {photoUrl}=useSelector(state=>state.auth?.user)
 
   return (
     <div className='border border-dark header'>
@@ -37,7 +36,7 @@ const Headers = ({ handleToggleSideBar }) => {
         <MdNotifications size={28} />
         <MdApps size={28} />
         <img
-          src={photoURL}
+          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
           alt="avatar"
         />
       </div>
